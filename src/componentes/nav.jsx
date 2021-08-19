5dbed1e7e1e504f1b82eaa25b64b06ee8b664eb2
Leidy Paola <paoalm1@hotmail.com>
2021-08-19 12:01:36 -0500

    App

---
+++ src/componentes/nav.jsx
@@ -0,0 +1,29 @@
+import '../style/nav.css';
+import React, {useState} from 'react';
+
+function Nav() {
+const [value, setValue]=useState("")
+
+const valueChange=(e)=>{
+	setValue(e.target.value)
+	console.log(e.target.value)
+}
+	return (
+		<nav className="nav">
+			<span className="namePage"> Movies</span>
+			<div>
+				<input type="search"value={value} onChange={valueChange}></input>
+				<button type="submit"> Buscar</button>
+			</div>
+			<ul className="lista">
+				<li className="login"> Iniciar sesion</li>
+				<li className="signup">
+					Registrarme
+				</li>
+			</ul>
+
+		</nav>
+	);
+}
+
+export default Nav;
