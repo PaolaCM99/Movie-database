df3af25646ec22dac575bc2aa3a4aa29007800fd
Leidy Paola <paoalm1@hotmail.com>
2021-09-24 15:35:12 -0500

    buscador

--- src/componentes/movies.jsx
+++ src/componentes/movies.jsx
@@ -1,42 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import '../style/movies.css'
-function Movies() {
-	const [movies, setMovies] = useState([])
-	const APIKEY = "6de2a6f114eee5868a9274ad2583836c"
-	const principalUrl = "https://api.themoviedb.org/3/"
-//    const imageUrl="https://https://image.tmdb.org/t/p/"
-//    const size = "w500"
-	const url = `${principalUrl}discover/movie?sort_by=popularity.des&api_key=${APIKEY}`;
-	const fecthUrl = async () => {
-		const data = await fetch(url)
-		const dataJson = await data.json()
-		setMovies(dataJson.results)
-		
-	}
-	
-	useEffect(() => {
-		fecthUrl()
-		console.log(movies)
-	}, [])
+function Movies({movies}) {
 
 	return (
 		<main className="container-movies">
 			{movies.map((movie) => (
-				
-				<div className="movie">
+				<div key={movie.id} className="movie">
 					<p className="title">{movie.title}</p>
 					<div className="container-img">
-					<img className="img" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt=" Poster"/>
+						<img className="img" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt=" Poster" />
 					</div>
-					<div className="overview"> Overview: <br/>{movie.overview}</div>
-				
+					<div className="overview">
+						{movie.overview}
+					</div>
 				</div>
-				
-				)
+			)
 			)}
-		</main> 
-			
-		
+		</main>
 	);
 };
 
