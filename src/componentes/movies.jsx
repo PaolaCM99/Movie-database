456e6191a4485a4d0f8c1cc1ce55afed7eface6e
Leidy Paola <paoalm1@hotmail.com>
2021-09-24 23:00:33 -0500

    background modal

--- src/componentes/movies.jsx
+++ src/componentes/movies.jsx
@@ -15,29 +15,32 @@ function Movies({ movies }) {
 		<main className="container-movies" >
 			{movies.map((movie) => (
 				<>
-				<div key={movie.id} className="movie">
-					<div className="container-img" onClick={handleOpen}>
-						<img className="img" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt=" Poster" />
-						<p className="title">{movie.title}</p>
+					<div key={movie.id} className="movie">
+						<div className="container-img" onClick={handleOpen}>
+							<img className="img" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt=" Poster" />
+							<p className="title">{movie.title}</p>
+						</div>
+
 					</div>
-					
-				</div>
-				{modal === false ? "" :
-						(<div className="overview">
-							<i className="fas fa-times modal-close" onClick={handleClose}></i>
-							<article className="modal-information"> 
-							<h3>Overview</h3>
-							<p>{movie.overview} </p>
-							<h3> Fecha de lanzamiento</h3>
-							<p>{movie.release_date}</p>
-							</article>
-						</div>)
+					{modal === false ? "" :
+						(<div className="modal-background">
+							<div className="overview">
+								<i className="fas fa-times modal-close" onClick={handleClose}></i>
+								<article className="modal-information">
+									<h3>Overview</h3>
+									<p>{movie.overview} </p>
+									<h3> Fecha de lanzamiento</h3>
+									<p>{movie.release_date}</p>
+								</article>
+							</div>
+						</div>
+						)
 					}
 				</>
 			)
 			)}
 
-			
+
 		</main>
 	);
 };
